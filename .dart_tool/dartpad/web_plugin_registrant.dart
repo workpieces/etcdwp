6cc2cd7717112eaaeaf971d156eece38de051f3d
taoshumin_vendor <tsmidx@gmail.com>
2023-03-01 09:33:16 +0800

    feat: build test

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,20 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FilePickerWeb.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
